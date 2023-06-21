03f7f3eb37b8696f1a728caf60ea3d237f47493d
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-22 07:32:13 +1200

    Revert "Fixed errors"
    This reverts commit b5be241a4368ce9e0349e7569fcdbc94f13c0b77.

--- DoublyLinkedListWithErrors/DoublyLinkedListWithErrors/DLLNode.cs
+++ DoublyLinkedListWithErrors/DoublyLinkedListWithErrors/DLLNode.cs
@@ -6,25 +6,18 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedListWithErrors
 {
-    // Namespace declaration
-    // All code within this namespace belongs to the DoublyLinkedListWithErrors namespace
-    public class DLLNode
+   public class DLLNode
     {
-        public int num;               // field of the node to hold a number
-        public DLLNode next;          // pointer to the next node in the list
-        public DLLNode previous;      // pointer to the previous node in the list
-
-        // Constructor for DLLNode class
-        // Initializes the num field with the provided value and sets next and previous references to null
-        public DLLNode(int num)
+        public int num;   // field of the node
+        public DLLNode next; // pointer to the next node
+        public DLLNode previous; // pointer to the previous node
+        public DLLNode (int num)
         {
             this.num = num;
             next = null;
             previous = null;
-        }
+        } // end of constructor
 
-        // Method to check if a number is prime
-        // Returns true if the number is prime, false otherwise
         public Boolean isPrime(int n)
         {
             Boolean b = true;
@@ -35,8 +28,6 @@ namespace DoublyLinkedListWithErrors
             }
             else
             {
-                // Loop from 2 to the square root of n
-                // Check if there is any divisor for n
                 for (int i = 2; i < Math.Sqrt(n); i++)
                 {
                     if ((n % i) == 0)
@@ -46,8 +37,8 @@ namespace DoublyLinkedListWithErrors
                     }
                 }
             }
-
             return (b);
-        }
-    }
+        } // end of isPrime
+
+    } // end of class DLLNode
 }
--- DoublyLinkedListWithErrors/DoublyLinkedListWithErrors/DLList.cs
+++ DoublyLinkedListWithErrors/DoublyLinkedListWithErrors/DLList.cs
@@ -6,38 +6,19 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedListWithErrors
 {
-    public class DLLNode
-    {
-        public int num; // data stored in the node
-        public DLLNode next; // pointer to the next node
-        public DLLNode previous; // pointer to the previous node
-
-        public DLLNode(int num)
-        {
-            this.num = num;
-            this.next = null;
-            this.previous = null;
-        }
-    }
-
-    public class DLList
+   public class DLList
     {
         public DLLNode head; // pointer to the head of the list
         public DLLNode tail; // pointer to the tail of the list
-
-        public DLList()
-        {
-            head = null;
-            tail = null;
-        }
-
+       public DLList() { head = null;  tail = null; } // constructor
         /*-------------------------------------------------------------------
-        * The methods below includes several errors. Your task is to write
-        * unit tests to discover these errors. During delivery, the tutor may
-        * add or remove errors to adjust the scale of the effort required.
+        * The methods below includes several errors. Your  task is to write
+        * unit test to discover these errors. During delivery the tutor may
+        * add or remove errors to adjust the scale of the effort required by
         */
         public void addToTail(DLLNode p)
         {
+
             if (head == null)
             {
                 head = p;
@@ -46,10 +27,10 @@ namespace DoublyLinkedListWithErrors
             else
             {
                 tail.next = p;
-                p.previous = tail;
                 tail = p;
+                p.previous = tail;
             }
-        }
+        } // end of addToTail
 
         public void addToHead(DLLNode p)
         {
@@ -60,79 +41,67 @@ namespace DoublyLinkedListWithErrors
             }
             else
             {
-                p.next = head;
-                head.previous = p;
+                p.next = this.head;
+                this.head.previous = p;
                 head = p;
             }
-        }
+        } // end of addToHead
 
-        public void removeHead()
+        public void removHead()
         {
-            if (head == null)
-                return;
-
-            if (head == tail)
-            {
-                head = null;
-                tail = null;
-            }
-            else
-            {
-                head = head.next;
-                head.previous = null;
-            }
-        }
+            if (this.head == null) return;
+            this.head = this.head.next;
+            this.head.previous = null;
+        } // removeHead
 
         public void removeTail()
         {
-            if (tail == null)
+            if (this.tail == null) return;
+            if (this.head == this.tail)
+            {
+                this.head = null;
+                this.tail = null;
                 return;
-
-            if (head == tail)
-            {
-                head = null;
-                tail = null;
             }
-            else
-            {
-                tail = tail.previous;
-                tail.next = null;
-            }
-        }
+        } // remove tail
 
         /*-------------------------------------------------
-         * Return null if the node with the given number does not exist.
+         * Return null if the string does not exist.
          * ----------------------------------------------*/
         public DLLNode search(int num)
         {
             DLLNode p = head;
             while (p != null)
             {
-                if (p.num == num)
-                    return p;
-
                 p = p.next;
+                if (p.num == num) break;
             }
-            return null;
-        }
+            return (p);
+        } // end of search (return pionter to the node);
 
         public void removeNode(DLLNode p)
-        {
-            if (p == null)
-                return;
+        { // removing the node p.
 
-            if (p == head)
-                removeHead();
-            else if (p == tail)
-                removeTail();
-            else
+            if (p.next == null)
             {
-                p.previous.next = p.next;
-                p.next.previous = p.previous;
-                p.next = null;
+                this.tail = this.tail.previous;
+                this.tail.next = null;
                 p.previous = null;
+                return;
             }
-        }
+            if (p.previous == null)
+            {
+                this.head = this.head.next;
+                p.next = null;
+                this.head.previous = null;
+                return;
+            }
+            p.next.previous = p.previous;
+            p.previous.next = p.next;
+            p.next = null;
+            p.previous = null;
+            return;
+        } // end of remove a node
 
         public int total()
         {
@@ -141,9 +110,9 @@ namespace DoublyLinkedListWithErrors
             while (p != null)
             {
                 tot += p.num;
-                p = p.next;
+                p = p.next.next;
             }
-            return tot;
-        }
-    }
+            return (tot);
+        } // end of total
+    } // end of DLList class
 }
